732e68e3446c5fe2ab7e3fb4cb83334e13a89fb9
Tin Svagelj <tin.svagelj@live.com>
2021-08-19 20:14:04 +0200

    Removed ua-parser-js dependency.

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,18 +1,15 @@
-import UAParser from 'ua-parser-js';
-
 interface UserInfo {
-    mobile: boolean;
-    appleWebKit: boolean;
+  mobile: boolean;
+  appleWebKit: boolean;
 }
 
 function userInfo(): UserInfo {
-    const UA = window && window.navigator && window.navigator.userAgent || '';
-    const UAP = new UAParser(UA);
+  const UA = (window && window.navigator && window.navigator.userAgent) || "";
 
-    return {
-        mobile: UAP.getDevice().type === 'mobile', // donut is sadly terrible on mobile
-        appleWebKit: UA.indexOf('AppleWebKit') > -1, // SVG animation is/was slow in Safari based browsers
-    }
+  return {
+    mobile: UA.match(/(iPhone|iPod|iPad|Android|BlackBerry)/).length > 0, // donut is sadly terrible on mobile
+    appleWebKit: UA.indexOf("AppleWebKit") > -1, // SVG animation is/was slow in Safari based browsers
+  };
 }
 
 export default userInfo;
